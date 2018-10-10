83172341631c80c92339eccc597150147461b5f4
asbruno <andradesouza.bruno@gmail.com>
2018-10-09 22:52:41 -0300

    commit do arquivo README.txt

---
+++ restful-desafio-solutis/bin/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into resultado_analise (string, vogal, tempo_total) values ('aAbBABacafe', 'e', 10);
+insert into resultado_analise (string, vogal, tempo_total) values ('aAbBABiacafe', 'i', 156);
